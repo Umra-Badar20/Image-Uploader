2ddd7565cb5069488bff731baf59cebf78be7769
Umra-Badar20 <umrabadar555@gamilo.com>
2026-04-27 15:54:28 +0500

    local storage integrated

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-var imageInput = document.getElementById("imgInput")
-var previewImg =  document.getElementById("previewImg")
+var imageInput = document.getElementById("imgInput");
+var previewImg = document.getElementById("previewImg");
 
-imageInput.addEventListener("change",function(){
-    var file = imageInput.files[0] 
-    if(file){
-        var reader= new FileReader()
-        console.log(reader);
-        reader.onload = function(e){
-            // console.log("event",e.target.result);
-            previewImg.src = e.target.result
-            previewImg.style.display = "block"
-            
-        }
-    }   
-    reader.readAsDataURL(file)    
-})
+window.onload = function () {
+  var savedImg = localStorage.getItem("imgData");
+  if (savedImg) {
+    previewImg.src = savedImg;
+    previewImg.style.display = "block";
+  }
+};
+
+imageInput.addEventListener("change", function () {
+  var file = imageInput.files[0];
+  if (file) {
+    var reader = new FileReader();
+    console.log(reader);
+    reader.onload = function (e) {
+      // console.log("event",e.target.result);
+      previewImg.src = e.target.result;
+      previewImg.style.display = "block";
+      localStorage.setItem("imgData", e.target.result);
+    };
+  }
+  reader.readAsDataURL(file);
+});
